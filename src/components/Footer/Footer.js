494d2b7e4618eb30e7ee27ef1b5b4aff296725a5
Junaid <qjunaid623@gmail.com>
2020-09-23 16:52:45 +0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,57 +6,57 @@ function Footer() {
     <div className="footer-container">
       <hr />
       <hr />
-      <div class="page-wrapper"></div>
+      <div className="page-wrapper"></div>
       <footer>
-        <div class="footer">
-          <div class="footer-content">
-            <div class="footer-section about">
-              <h1 class="logo-text">
-                <span class="logo-span">J</span>unaid &nbsp;
-                <span class="logo-span">Q</span>ureshi
+        <div className="footer">
+          <div className="footer-content">
+            <div className="footer-section about">
+              <h1 className="logo-text">
+                <span className="logo-span">J</span>unaid &nbsp;
+                <span className="logo-span">Q</span>ureshi
               </h1>
-              <p class="footer-para">
+              <p className="footer-para">
                 I am a creative, Web Developer with command on Latest Tech
                 stacks
               </p>
-              <div class="contact">
+              <div className="contact">
                 <span>
-                  <i class="fas fa-envelope"></i>&nbsp; qjunaid623@gmail.com
+                  <i className="fas fa-envelope"></i>&nbsp; qjunaid623@gmail.com
                 </span>
               </div>
-              <div class="socials">
+              <div className="socials">
                 <a
                   href="https://www.facebook.com/profile.php?id=100041078220896"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i class="fab fa-facebook"></i>
+                  <i className="fab fa-facebook"></i>
                 </a>
                 <a
                   href="https://www.linkedin.com/in/junaid-qureshi-34a180194/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  <i class="fab fa-linkedin"></i>
+                  <i className="fab fa-linkedin"></i>
                 </a>
                 <a
                   href="https://twitter.com/Junaid084869/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  <i class="fab fa-twitter"></i>
+                  <i className="fab fa-twitter"></i>
                 </a>
                 <a
                   href="https://github.com/developer-junaid"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  <i class="fab fa-github"></i>
+                  <i className="fab fa-github"></i>
                 </a>
               </div>
             </div>
           </div>
-          <div class="footer-bottom">&copy; | Designed By Junaid Qureshi</div>
+          <div className="footer-bottom">&copy; | Designed By Junaid Qureshi</div>
         </div>
       </footer>
     </div>
